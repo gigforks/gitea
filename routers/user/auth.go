cf231015a080eaa92c3200fa741894e564b8d28a
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2017-12-19 18:47:27 +0200

    Remove deleting goth session from logout

--- routers/user/auth.go
+++ routers/user/auth.go
@@ -742,21 +742,6 @@ func LinkAccountPostRegister(ctx *context.Context, cpt *captcha.Captcha, form au
 
 // SignOut sign out from login status
 func SignOut(ctx *context.Context) {
-	// Delete OAUTH2 sessions also
-	if ctx.IsSigned && ctx.User.LoginType == models.LoginOAuth2 {
-		loginSource , err:= models.GetLoginSourceByID(ctx.User.LoginSource)
-		if err == nil{
-			sessionKey := loginSource.Name + gothic.SessionName
-			session, err := gothic.Store.Get(ctx.Req.Request, sessionKey)
-			if err == nil {
-				session.Options.MaxAge = -1
-				session.Values = make(map[interface{}]interface{})
-				session.Save(ctx.Req.Request, ctx.Resp)
-			}
-			ctx.SetCookie(sessionKey, "", -1, setting.AppSubURL)
-		}
-	}
-
 	ctx.Session.Delete("uid")
 	ctx.Session.Delete("uname")
 	ctx.Session.Delete("socialId")
